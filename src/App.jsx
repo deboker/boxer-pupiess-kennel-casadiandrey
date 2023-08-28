a56a647b34bc28466139e6f567b76d19f9307672
deboker <117391017+deboker@users.noreply.github.com>
2023-08-28 21:33:28 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import Loader from "./components/Loader/Loader";
 
 export default function App() {
-  return (
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fakeDataFech = () => {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 4000);
+    };
+    fakeDataFech();
+  });
+
+  return isLoading ? (
+    <Loader />
+  ) : (
     <div>
       <Header />
       <Body />
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./loader.css";
+// import logo from "./assets/logo.jpeg";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="svg-wraper">
+        <div className="loading-text"></div>
+        <p>LOGO TU MA IST</p>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
