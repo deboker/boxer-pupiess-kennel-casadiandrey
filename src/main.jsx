68492a78851995741deda8a5deaf37efb8e3e00c
deboker <117391017+deboker@users.noreply.github.com>
2023-11-15 08:57:14 +0100

    ff

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "react-toggle-dark-mode/dist/toggle-style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
