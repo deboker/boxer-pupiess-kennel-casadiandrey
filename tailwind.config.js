98413a0590935a03ecf058eee23335272709e40a
deboker <117391017+deboker@users.noreply.github.com>
2024-10-15 22:23:16 +0200

    new contact form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,113 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: 'class', // This sets dark mode to be controlled by adding 'dark' class in the HTML
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", 
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'primary': 'var(--bg-color)',
-        'secondary': 'var(--second-bg-color)',
-        'primary-dark': 'var(--bg-color-dark-mode)',
-        'secondary-dark': 'var(--second-bg-color-dark-mode)',
-        'btn-color': 'var(--btn-color)',
-        'text-color': 'var(--text-color)',
-        'text-color-dark': 'var(--text-color-dark-mode)',
-      },
-      backgroundColor: {
-        'gradient-top': 'linear-gradient(to top, var(--bg-color), var(--second-bg-color))',
-        'gradient-bottom': 'linear-gradient(to bottom, var(--bg-color), var(--second-bg-color))',
-        'gradient-top-dark': 'linear-gradient(to top, var(--bg-color-dark-mode), var(--second-bg-color-dark-mode))',
-        'gradient-bottom-dark': 'linear-gradient(to bottom, var(--bg-color-dark-mode), var(--second-bg-color-dark-mode))',
-      }
-    }
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+import React from "react";
+
+const ContactForm = () => {
+  return (
+    <>
+      <section
+        className="bg-gradient-top dark:bg-gradient-top-dark text-text-color dark:text-text-color-dark"
+        id="contact"
+      >
+        <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+          <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
+            <div className="lg:col-span-2 lg:py-12">
+              <div className="mt-8 text-center">
+                <a href="#" className="text-2xl font-bold text-pink-600">
+                  Contact us
+                </a>
+              </div>
+              <p className="max-w-xl text-lg text-text-color dark:text-text-color-dark">
+                Reserve Your Dog Now! Contact us for more photos, short videos
+                showcasing the dog, health certificates, and a rating list from
+                exhibitions. We also offer images of prize cups our champions
+                have won. Discover Your New Furry Friend and embark on a
+                journey.
+              </p>
+            </div>
+
+            <div className="rounded-lg p-8 shadow-lg lg:col-span-3 lg:p-12">
+              <form
+                name="contact"
+                method="POST"
+                action="/thanks"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                className="space-y-4"
+              >
+                <input type="hidden" name="form-name" value="contact" />
+
+                <div>
+                  <label className="sr-only" htmlFor="name">
+                    Name
+                  </label>
+                  <input
+                    name="name"
+                    className="w-full rounded-lg border-gray-200 p-3 text-sm dark:bg-gray-800 dark:text-white"
+                    placeholder="Name"
+                    type="text"
+                    id="name"
+                    required
+                  />
+                </div>
+
+                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                  <div>
+                    <label className="sr-only" htmlFor="email">
+                      Email
+                    </label>
+                    <input
+                      name="email"
+                      className="w-full rounded-lg border-gray-200 p-3 text-sm dark:bg-gray-800 dark:text-white"
+                      placeholder="Email address"
+                      type="email"
+                      id="email"
+                      required
+                    />
+                  </div>
+
+                  <div>
+                    <label className="sr-only" htmlFor="phone">
+                      Phone
+                    </label>
+                    <input
+                      name="phone"
+                      className="w-full rounded-lg border-gray-200 p-3 text-sm bg-gradient-bottom dark:bg-gradient-bottom-dark dark:text-white"
+                      placeholder="Phone Number"
+                      type="tel"
+                      id="phone"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label className="sr-only" htmlFor="message">
+                    Message
+                  </label>
+
+                  <textarea
+                    name="message"
+                    className="resize-none w-full rounded-lg border-gray-200 p-3 text-sm bg-gradient-top dark:bg-gradient-top-dark dark:text-white"
+                    placeholder="Message"
+                    rows="8"
+                    id="message"
+                    required
+                  ></textarea>
+                </div>
+
+                <div className="mt-4">
+                  <button
+                    type="submit"
+                    className="inline-block w-full rounded-lg bg-btn-color px-5 py-3 font-medium text-white sm:w-auto dark:bg-gray-700"
+                  >
+                    Send Enquiry
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ContactForm;
