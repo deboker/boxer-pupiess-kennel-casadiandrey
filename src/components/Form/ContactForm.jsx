17e44896506a828b7794b0605543f952de191f45
deboker <117391017+deboker@users.noreply.github.com>
2024-10-16 17:23:47 +0200

    ssss

--- src/components/Form/ContactForm.jsx
+++ src/components/Form/ContactForm.jsx
@@ -11,11 +11,11 @@ const ContactForm = () => {
           <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
             <div className="lg:col-span-2 lg:py-12">
               <div className="mt-8 text-center">
-                <a href="#" className="text-2xl font-bold text-pink-600">
+                <a href="#" className="text-2xl font-bold text-primary">
                   Contact us
                 </a>
               </div>
-              <p className="max-w-xl text-lg text-red-700">
+              <p className="max-w-xl text-lg text-primary">
                 Reserve Your Dog Now! Contact us for more photos, short videos,
                 health certificates, and a rating list from exhibitions. We also
                 offer images of prize cups our champions have won.
