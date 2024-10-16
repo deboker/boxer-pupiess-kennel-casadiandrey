a886342f65b92d467d0126c5b14789ff3a11a15e
deboker <117391017+deboker@users.noreply.github.com>
2024-10-16 13:28:50 +0200

    fixing the bg colors in contact section

--- src/components/Form/ContactForm.jsx
+++ src/components/Form/ContactForm.jsx
@@ -3,10 +3,7 @@ import React from "react";
 const ContactForm = () => {
   return (
     <>
-      <section
-        className="bg-gradient-top dark:bg-gradient-top-dark"
-        id="contact"
-      >
+      <section className="bg-primary dark:bg-gradient-top-dark" id="contact">
         <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
             <div className="lg:col-span-2 lg:py-12">
