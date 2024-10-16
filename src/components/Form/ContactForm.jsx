3f03722efcc5dd268c07a5bcc0eb4e590f5bac0a
deboker <117391017+deboker@users.noreply.github.com>
2024-10-16 11:20:34 +0200

    test new contact section

--- src/components/Form/ContactForm.jsx
+++ src/components/Form/ContactForm.jsx
@@ -3,112 +3,57 @@ import React from "react";
 const ContactForm = () => {
   return (
     <>
-      <section
-        className="bg-gradient-top dark:bg-gradient-top-dark"
-        id="contact"
-      >
-        <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-          <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
-            <div className="lg:col-span-2 lg:py-12">
-              <div className="mt-8 text-center">
-                <a href="#" className="text-2xl font-bold text-pink-600">
-                  Contact us
-                </a>
-              </div>
-              <p className="max-w-xl text-lg text-red-700">
-                Reserve Your Dog Now! Contact us for more photos, short videos,
-                health certificates, and a rating list from exhibitions. We also
-                offer images of prize cups our champions have won.
-              </p>
-            </div>
+      <section class="contact" id="contact">
+        <h2 class="heading">
+          Contact <span>Me!</span>
+        </h2>
 
-            <div className="rounded-lg p-8 shadow-lg lg:col-span-3 lg:p-12">
-              <form
-                name="contact"
-                netlify
-                method="POST"
-                action="/thank-you"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                id="contact-form"
-              >
-                <input type="hidden" name="form-name" value="contact" />
-                <p className="hidden">
-                  <label>
-                    Don't fill this out if you're human:{" "}
-                    <input name="bot-field" />
-                  </label>
-                </p>
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit="submit"
+          id="contact-form"
+          netlify
+        >
+          <input class="d-none" name="bot-field" />
 
-                <div className="w-9/12 mx-auto">
-                  <label className="sr-only" htmlFor="name">
-                    Name
-                  </label>
-                  <input
-                    name="name"
-                    className="w-full rounded-lg border-gray-200 p-3 text-sm bg-white dark:bg-gray-900 dark:text-white"
-                    placeholder="Full Name"
-                    type="text"
-                    id="name"
-                    required
-                  />
-                </div>
-
-                <div className="w-9/12 mx-auto grid grid-cols-1 gap-4 sm:grid-cols-2">
-                  <div>
-                    <label className="sr-only" htmlFor="email">
-                      Email
-                    </label>
-                    <input
-                      name="email"
-                      className="w-full rounded-lg border-gray-200 p-3 text-sm bg-white dark:bg-gray-900 dark:text-white"
-                      placeholder="Email Address"
-                      type="email"
-                      id="email"
-                      required
-                    />
-                  </div>
-
-                  <div>
-                    <label className="sr-only" htmlFor="phone">
-                      Phone
-                    </label>
-                    <input
-                      name="phone"
-                      className="w-full rounded-lg border-gray-200 p-3 text-sm bg-white dark:bg-gray-900 dark:text-white"
-                      placeholder="Phone Number"
-                      type="tel"
-                      id="phone"
-                    />
-                  </div>
-                </div>
-
-                <div className="w-9/12 mx-auto">
-                  <label className="sr-only" htmlFor="message">
-                    Message
-                  </label>
-                  <textarea
-                    name="message"
-                    className="resize-none w-full rounded-lg border-gray-200 p-3 text-sm bg-gradient-top dark:bg-gradient-top-dark dark:text-white"
-                    placeholder="Your Message"
-                    rows="8"
-                    id="message"
-                    required
-                  ></textarea>
-                </div>
-
-                <div className="mt-4">
-                  <button
-                    type="submit"
-                    className="inline-block w-full rounded-lg bg-btn-color px-5 py-3 font-medium text-white dark:bg-gray-700 sm:w-auto"
-                  >
-                    Send Message
-                  </button>
-                </div>
-              </form>
-            </div>
+          <div class="input-box">
+            <input
+              type="text"
+              name="user_name"
+              placeholder="Full Name"
+              required
+            />
+            <input
+              type="email"
+              name="user_email"
+              placeholder="Email Address"
+              required
+            />
           </div>
-        </div>
+          <div class="input-box">
+            <input
+              type="number"
+              name="user_phone"
+              placeholder="Mobile Number"
+            />
+            <input
+              type="text"
+              name="email_subject"
+              placeholder="Email Subject"
+            />
+          </div>
+          <textarea
+            name="message"
+            cols="30"
+            rows="10"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <input type="submit" value="Send Message" class="btn" />
+        </form>
       </section>
     </>
   );
