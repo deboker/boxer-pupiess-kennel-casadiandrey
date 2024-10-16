d13505687de75658df258cb4f5743de10601fff4
deboker <117391017+deboker@users.noreply.github.com>
2024-10-16 10:11:08 +0200

    ssaaa

--- src/components/Form/ContactForm.jsx
+++ src/components/Form/ContactForm.jsx
@@ -4,18 +4,21 @@ const ContactForm = () => {
   return (
     <>
       <section
-        className="bg-red-600 text-red-400 bg-gradient-top dark:bg-gradient-top-dark"
+        className="bg-gradient-top text-text-color dark:bg-gradient-top-dark dark:text-text-color-dark"
         id="contact"
       >
         <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
             <div className="lg:col-span-2 lg:py-12">
               <div className="mt-8 text-center">
-                <a href="#" className="text-2xl font-bold text-pink-600">
+                <a
+                  href="#"
+                  className="text-2xl font-bold text-pink-600 dark:text-pink-400"
+                >
                   Contact us
                 </a>
               </div>
-              <p className="max-w-xl text-lg text-red-700">
+              <p className="max-w-xl text-lg text-secondary dark:text-secondary-dark">
                 Reserve Your Dog Now! Contact us for more photos, short videos
                 showcasing the dog, health certificates, and a rating list from
                 exhibitions. We also offer images of prize cups our champions
@@ -24,7 +27,7 @@ const ContactForm = () => {
               </p>
             </div>
 
-            <div className="rounded-lg p-8 shadow-lg lg:col-span-3 lg:p-12">
+            <div className="rounded-lg p-8 shadow-lg lg:col-span-3 lg:p-12 bg-white dark:bg-gray-800">
               <form
                 name="contact"
                 method="POST"
@@ -35,7 +38,6 @@ const ContactForm = () => {
               >
                 <input type="hidden" name="form-name" value="contact" />
 
-                {/* Wrap the div with max-w-full to make the width more flexible */}
                 <div className="max-w-full">
                   <div className="w-9/12 mx-auto">
                     <label className="sr-only" htmlFor="name">
@@ -43,7 +45,7 @@ const ContactForm = () => {
                     </label>
                     <input
                       name="name"
-                      className="w-full rounded-lg border-gray-200 p-3 text-sm dark:bg-gray-800 dark:text-white"
+                      className="w-full rounded-lg border-gray-200 p-3 text-sm bg-white dark:bg-gray-900 dark:text-white"
                       placeholder="Name"
                       type="text"
                       id="name"
@@ -60,7 +62,7 @@ const ContactForm = () => {
                       </label>
                       <input
                         name="email"
-                        className="w-full rounded-lg border-gray-200 p-3 text-sm dark:bg-gray-800 dark:text-white"
+                        className="w-full rounded-lg border-gray-200 p-3 text-sm bg-white dark:bg-gray-900 dark:text-white"
                         placeholder="Email address"
                         type="email"
                         id="email"
@@ -74,7 +76,7 @@ const ContactForm = () => {
                       </label>
                       <input
                         name="phone"
-                        className="bg-gradient-background-botton w-full rounded-lg border-gray-200 p-3 text-sm dark:bg-gray-800 dark:text-white"
+                        className="w-full rounded-lg border-gray-200 p-3 text-sm bg-white dark:bg-gray-900 dark:text-white"
                         placeholder="Phone Number"
                         type="tel"
                         id="phone"
@@ -103,7 +105,7 @@ const ContactForm = () => {
                 <div className="mt-4">
                   <button
                     type="submit"
-                    className="inline-block w-full rounded-lg bg-btn-color px-5 py-3 font-medium text-white sm:w-auto"
+                    className="inline-block w-full rounded-lg bg-btn-color px-5 py-3 font-medium text-white dark:bg-gray-700 sm:w-auto"
                   >
                     Send Enquiry
                   </button>
