b0b7ebd9a0fffd39e353c9525fb991135a770cc7
deboker <117391017+deboker@users.noreply.github.com>
2026-03-07 21:28:05 +0100

    Add available puppies section and replace photo assets

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import Lottie from "lottie-react";
 import ContactForm from "./Form/ContactForm";
 import meting from "../assets/aria&xxx.jpg";
+
 import facebookIconAnimation from "../assets/lottieflow-social-networks-15-3-000000-easey.json";
 import intstagramIconAnimation from "../assets/lottieflow-social-networks-15-5-000000-easey.json";
 import arrow from "../assets/arrow-up.svg";
@@ -83,6 +84,59 @@ export default function Body(props) {
           health-tested sires and dams, we prioritize the well-being of each
           pup..
         </p>
+      </section>
+
+      <section className={`available ${props.darkMode ? "dark-mode" : ""}`}>
+        <h1>AVAILABLE PUPPIES</h1>
+        <div className="video-galery">
+          <h2>Meet Adam d'Black.</h2>
+
+          <img
+            className="feature-video"
+            src={meting}
+            alt="Adam d'Black Casa di Andrey"
+          />
+          {/* <p className="available-label">Available</p> */}
+        </div>
+
+        <div className={`feature-item ${props.darkMode ? "dark-mode" : ""}`}>
+          <img src={meting} alt="Adam d black head" className="feature-image" />
+
+          {/* <p className="reserved-label">Puppy Reserved</p> */}
+          {/* <p className="available-label">Available</p> */}
+          <p className="image-text"> Casa di Andrey</p>
+        </div>
+        <div className={`feature-item ${props.darkMode ? "dark-mode" : ""}`}>
+          <img src={meting} alt="Adam d black side" className="feature-image" />
+          {/* <p className="reserved-label">Puppy Reserved</p> */}
+          {/* <p className="available-label">Available</p> */}
+          <p className="image-text"> Casa di Andrey</p>
+        </div>
+        <div className={`feature-item ${props.darkMode ? "dark-mode" : ""}`}>
+          <img src={meting} alt="Adam d black runs" className="feature-image" />
+          {/* <p className="reserved-label">Puppy Reserved</p> */}
+          {/* <p className="available-label">Available</p> */}
+          <p className="image-text"> Casa di Andrey</p>
+        </div>
+        <div className={`feature-item ${props.darkMode ? "dark-mode" : ""}`}>
+          <img
+            src={meting}
+            alt="Adam d black puppy1"
+            className="feature-image"
+          />
+          {/* <p className="reserved-label">Puppy Reserved</p> */}
+          {/* <p className="available-label">Available</p> */}
+          <p className="image-text">Puppy Casa di Andrey</p>
+        </div>
+        <div className="reserve-text">
+          <p>
+            Reserve Your Dog Now! Contact us for more photos, short videos
+            showcasing the dog, health certificates, and a rating list from
+            exhibitions. We also offer images of prize cups our champions have
+            won. Discover your new furry friend and embark on a journey.
+          </p>
+          <p>Stay tuned for our upcoming litters too!</p>
+        </div>
       </section>
 
       {/* Contact Form */}
