da84575212ffba08712caf298fdb76ecdcce68ba
deboker <117391017+deboker@users.noreply.github.com>
2026-03-14 09:20:36 +0100

    females two videos

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,8 +3,8 @@ import Lottie from "lottie-react";
 import ContactForm from "./Form/ContactForm";
 import meting from "../assets/aria-vlajko-meet-new.webp";
 import maleFawnVideo from "../assets/male_egon.mp4";
-import femaleBrindleVideo from "../assets/female_brindle.mp4";
-import femaleFawnVideo from "../assets/female_fawn.mp4";
+import femaleBrindleVideo from "../assets/female_aurora.mp4";
+import femaleFawnVideo from "../assets/female_nova.mp4";
 import maleBrindleVideo from "../assets/male_titan.mp4";
 import maleBrindleWhiteMaskVideo from "../assets/male_magnus.mp4";
 
